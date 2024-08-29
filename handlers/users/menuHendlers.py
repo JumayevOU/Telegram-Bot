9cf55150e033f970ea1fb7439357532293c908f7
JumayevOU <jumayevou@gmail.com>
2024-08-29 16:29:25 +0300

    nimadir qilindi

--- handlers/users/menuHendlers.py
+++ handlers/users/menuHendlers.py
@@ -1,7 +1,7 @@
 import logging 
 from aiogram.types import Message, CallbackQuery
 from loader import dp
-from keyboards.inline.Social_networks import back_button, social_network_Keyboard, main_menu, back_button_bio, resume_Keyboard,back_button_resume, back_button_network
+from keyboards.inline.Social_networks import social_network_Keyboard, main_menu, back_button_bio, resume_Keyboard,back_button_resume
 from keyboards.inline.startMenu import startMenu_Keyboard
 from aiogram.types import InputFile
 
@@ -20,14 +20,14 @@ async def select_category(call: CallbackQuery):
 @dp.callback_query_handler(text='bio')
 async def select_category(call: CallbackQuery):
   
-    photo_file = InputFile(path_or_bytesio="/home/acer/Downloads/Telegram Desktop/photo_2024-04-07_18-13-25.jpg")
+    photo_file = InputFile(path_or_bytesio="photos/O.B.jpg")
     msg = "<b>Ismi-sharifi:</b>\nOtajon Bozorboyev\n\n"
     msg += "<b>Tug'ilgan yili va joyi: </b>\n08-yanvar 1999-yil; \nJizzax viloyati, Jizzax shahri.\n\n"
     msg += "<b>Ta'limi:</b>\nToshkent temir yo'l transport kasb-hunar kolleji (2018)\n\n"
     msg += "<b>Ish tajribasi: :</b>\n-O'zbekiston temir yo\'llari AJ tashkiloti Jizzax temir yo'l masofasi xodimlar bo\'limi nazoratchisi (HR) (2018-2023);Astro Education Python Beckend Mentor(2023-2024);Tramplin IT Academy Backend dasturchi va Python backend mentor (2024-hozirgacha)\n\n"
     msg += "<b>Texnik ko'nikmalari: </b>\nC, Python, Django, Django Rest, SQLite, MySQL, PostgreSQL, Git, GitHub, HTML, CSS, Java Script, Telegram Bot, Microsoft Office(Word, Excel, Power Point, Paint, va h.k.lar)n\n"
     msg += "<b>Tillar: </b>\nO'zbek tili (Ona tili);\nIngliz tili (B2);\nArab tili (O'qiy olish);\nYapon tili (N3).\n\n"
-    await call.message.reply_photo(photo_file, caption=msg, reply_markup=back_button_bio)
+    await call.message.answer_photo(photo_file, caption=msg, reply_markup=back_button_bio)
     await call.message.delete()
 
 
@@ -40,7 +40,7 @@ async def select_category(call: CallbackQuery):
 
 @dp.callback_query_handler(text='download')
 async def select_category(call: CallbackQuery):
-    with open("/home/acer/Downloads/Telegram Desktop/Otajon-Bozorboev-FlowCV-Resume-20240815.pdf", 'rb') as pdf_file:
+    with open("documents/resume.pdf", 'rb') as pdf_file:
         await call.message.answer_document(pdf_file, caption="Bu Otajon Bozorboyevning resumesi!", reply_markup=back_button_resume)
     await call.message.delete()
 
@@ -48,29 +48,27 @@ async def select_category(call: CallbackQuery):
 #SOCIAL NETWORKS
 @dp.callback_query_handler(text='social_networks')
 async def select_category(call: CallbackQuery):
-    photo_file = InputFile(path_or_bytesio="/home/acer/Downloads/Telegram Desktop/photo_2024-08-16_23-04-35.jpg")
-    await call.message.reply_photo(photo_file, reply_markup=social_network_Keyboard)
+    photo_file = InputFile(path_or_bytesio="photos/socialnetworks.jpg")
+    await call.message.answer_photo(photo_file, reply_markup=social_network_Keyboard)
     await call.message.delete()
-
-
-
-#BACK
-@dp.callback_query_handler(text='back')
-async def select_category(call: CallbackQuery):
-    await call.message.delete()
-    await call.message.answer(f"Sahifani tanlang ⤵️", reply_markup=startMenu_Keyboard)
-
-
 
 
 @dp.callback_query_handler(text='back_bio')
 async def select_category(call: CallbackQuery):
+    await call.message.answer("Kerakli menuni tanlang ⤵️", reply_markup=startMenu_Keyboard)
     await call.message.delete()
-    await call.message.answer(f"Sahifani tanlang ⤵️", reply_markup=startMenu_Keyboard)
+
+
+
+
+@dp.callback_query_handler(text='back')
+async def select_category(call: CallbackQuery):
+    await call.message.answer("Kerakli menuni tanlang ⤵️", reply_markup=startMenu_Keyboard)
+    await call.message.delete()
 
 
 
 @dp.callback_query_handler(text='back_resume')
 async def select_category(call: CallbackQuery):
+    await call.message.answer("Bo'limni tanlang ⤵️", reply_markup=resume_Keyboard)
     await call.message.delete()
-    await call.message.answer(f"Bo'limni tanlang ⤵️", reply_markup=resume_Keyboard)
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,46 +1,19 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
-from data.config import CHANNELS
-from keyboards.inline.subscription import channel_button
+from aiogram.dispatcher.filters import CommandStart
 from keyboards.inline.startMenu import Menu_Keyboard
 from aiogram.types import InputFile
-from loader import dp, bot
-from utils.misc import subscription
-import asyncio
-
-@dp.message_handler(commands=['start'])
-async def show_channels(message: types.Message):
-    channels_format = str()
-    for channel in CHANNELS:
-        chat = await bot.get_chat(channel)
-      
-    await message.answer(f"❗️ Bot to'liq ishlashi uchun quyidagi kanalimizga obuna bo'ling 👇 \n"
-                         f"{channels_format}",
-                         reply_markup=channel_button,
-                         disable_web_page_preview=True)
+from loader import dp
 
 
-@dp.callback_query_handler(text="check_subs")
-async def checker(call: types.CallbackQuery):
-    result = str()
-    for channel in CHANNELS:
-        status = await subscription.check(user_id=call.from_user.id,
-                                          channel=channel)
-        print(status)
-        channel = await bot.get_chat(channel)
-        if status:
-            initial_message = await call.message.answer("🎉 Obuna bo’ldingiz. Endi botni bemalol ishlatishingiz mumkun.")
-            await call.message.delete()
-            await asyncio.sleep(1)
-            await initial_message.delete()
-            photo_file = InputFile(path_or_bytesio="/home/acer/Downloads/Telegram Desktop/photo_2024-04-07_18-13-25.jpg")
-            await call.message.bot.send_photo(chat_id=call.message.chat.id, photo=photo_file,caption=f"Salom {call.message.from_user.full_name} botimizga xush kelibsiz!\nBu <b>Otajon Bozorboyev</b>ning shaxsiy boti\nqo'shimcha ma'lumot olish uchun <b>Menu</b> tugmasini bosing ⤵️", reply_markup=Menu_Keyboard)
-            await call.message.answer(result, disable_web_page_preview=True)
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    photo_file = InputFile(path_or_bytesio="photos/O.B.jpg")
+    await message.answer_photo( photo=photo_file,caption=f"Assalomu aleykum, {message.from_user.full_name}\n botimizga xush kelibsiz!\nBu <b>Otajon Bozorboyev</b>ning shaxsiy boti\nqo'shimcha ma'lumot olish uchun <b>Menu</b> tugmasini bosing ⤵️", reply_markup=Menu_Keyboard)
+    await message.delete()
+
+
           
-        else:
-            result += "Iltimos, kanallarimizga obuna bo'ling, \nKeyin botni ishlatishingiz mumkin❗️"
-            await call.answer(result, show_alert=True)
-
+   
 
 
 
